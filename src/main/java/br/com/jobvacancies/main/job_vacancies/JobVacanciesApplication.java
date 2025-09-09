196fc11f81ec4fd4e2f0a2d37a44c5b9ee860c04
xsalles <xs.salles@gmail.com>
2025-09-09 16:17:16 -0300

    feat: started the repo.

---
+++ src/main/java/br/com/jobvacancies/main/job_vacancies/JobVacanciesApplication.java
@@ -0,0 +1,13 @@
+package br.com.jobvacancies.main.job_vacancies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobVacanciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobVacanciesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jobvacancies/main/job_vacancies/JobVacanciesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jobvacancies.main.job_vacancies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobVacanciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
