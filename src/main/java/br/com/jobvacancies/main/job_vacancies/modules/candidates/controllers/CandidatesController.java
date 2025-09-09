8459d71255b2e1fa23005ef640756beb2fead372
xsalles <xs.salles@gmail.com>
2025-09-09 17:03:59 -0300

    feat: added the validation fields: 'email', 'password', 'username'.

--- src/main/java/br/com/jobvacancies/main/job_vacancies/modules/candidates/controllers/CandidatesController.java
+++ src/main/java/br/com/jobvacancies/main/job_vacancies/modules/candidates/controllers/CandidatesController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jobvacancies.main.job_vacancies.modules.candidates.model.CandidateModel;
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,8 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class CandidatesController {
     
     @PostMapping("/create")
-    public CandidateModel create(@RequestBody CandidateModel candidateModel) {  
-       return candidateModel; 
+    public void create(@Valid @RequestBody CandidateModel candidateModel) {  
+       System.out.println(candidateModel.getEmail()); 
     }
     
 }
--- src/main/java/br/com/jobvacancies/main/job_vacancies/modules/candidates/model/CandidateModel.java
+++ src/main/java/br/com/jobvacancies/main/job_vacancies/modules/candidates/model/CandidateModel.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -12,11 +13,16 @@ public class CandidateModel {
     
     private UUID id;
     private String name;
+    
+    @Pattern( regexp = "^(?!\\s*$).+", message = "O campo (username) não deve conter espaços" )
     private String username;
 
+    @Email( message = "O campo (email) deve conter um email válido" )
     private String email;
 
+    @Length( min = 6, max = 100, message = "O campo (password) deve conter entre 6 e 100 caracteres" )
     private String password;
+    
     private String description;
     private String curriculum;
 
