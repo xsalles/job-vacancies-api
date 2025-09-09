e7d4c2e91dc4639f962df0921ab842afba3f77c1
xsalles <xs.salles@gmail.com>
2025-09-09 20:58:07 -0300

    feat: added my credentials of database in application properties and added a application.properties.example to avoid expose this type of data

--- src/main/java/br/com/jobvacancies/main/job_vacancies/JobVacanciesApplication.java
+++ src/main/java/br/com/jobvacancies/main/job_vacancies/JobVacanciesApplication.java
@@ -2,8 +2,10 @@ package br.com.jobvacancies.main.job_vacancies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.jobvacancies.main.job_vacancies")
 public class JobVacanciesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/jobvacancies/main/job_vacancies/modules/candidates/model/CandidateModel.java
+++ src/main/java/br/com/jobvacancies/main/job_vacancies/modules/candidates/model/CandidateModel.java
@@ -1,20 +1,29 @@
 package br.com.jobvacancies.main.job_vacancies.modules.candidates.model;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidates")
 public class CandidateModel {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
     
-    @Pattern( regexp = "^(?!\\s*$).+", message = "O campo (username) não deve conter espaços" )
+    @Pattern( regexp = "\\S+", message = "O campo (username) não deve conter espaços" )
     private String username;
 
     @Email( message = "O campo (email) deve conter um email válido" )
@@ -26,4 +35,6 @@ public class CandidateModel {
     private String description;
     private String curriculum;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
