aab84c2e790a46ec2be90e31685e4a2e5926a301
xsalles <xs.salles@gmail.com>
2025-09-11 15:00:08 -0300

    feat: added the spring security in my java application and removed csrf

---
+++ src/main/java/br/com/jobvacancies/main/job_vacancies/infrastructure/security/SecurityConfig.java
@@ -0,0 +1,18 @@
+package br.com.jobvacancies.main.job_vacancies.infrastructure.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+}
